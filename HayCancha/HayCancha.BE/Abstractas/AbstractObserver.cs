006d0546e451aac8c749bc5edeab8b1b654a149e
Lucas Chmelik <lchmelik@cys.com.ar>
2021-07-10 17:14:18 -0300

    Se commitea el .bak

---
+++ HayCancha/HayCancha.BE/Abstractas/AbstractObserver.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace HayCancha.BE.Interfaces
+{
+    public abstract class AbstractObserver
+    {
+        List<Idiomable> _lstControles = new List<Idiomable>();
+
+        public void Suscribir(Idiomable oComponente)
+        {
+            _lstControles.Add(oComponente);
+        }
+
+        public void Desuscribir(Idiomable oComponente)
+        {
+            _lstControles.Remove(oComponente);
+        }
+
+        public void Notificar()
+        {
+            foreach (var oComponente in _lstControles)
+            {
+                oComponente.Update();   
+            }
+        }
+    }
+}
