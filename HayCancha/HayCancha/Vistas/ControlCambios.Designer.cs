0d63aa8806b8547f1f99a65b650e531c7f2f642c
Lucas Chmelik <lchmelik@cys.com.ar>
2021-09-26 10:33:23 -0300

    Se modifica nombre de vista y se sube el .bak

---
+++ HayCancha/HayCancha/Vistas/ControlCambios.Designer.cs
@@ -0,0 +1,210 @@
+﻿
+namespace HayCancha.Vistas
+{
+    partial class ControlCambios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.lblCambios = new System.Windows.Forms.Label();
+            this.dgvUsuarios = new System.Windows.Forms.DataGridView();
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.btnRestaurarUsuario = new System.Windows.Forms.Button();
+            this.bunifuElipse3 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.lblBuscarUsuario = new System.Windows.Forms.Label();
+            this.imgBuscar = new System.Windows.Forms.PictureBox();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.bunifuElipse4 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsuarios)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBuscar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblCambios
+            // 
+            this.lblCambios.AutoSize = true;
+            this.lblCambios.Font = new System.Drawing.Font("Impact", 22F);
+            this.lblCambios.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblCambios.Location = new System.Drawing.Point(274, 43);
+            this.lblCambios.Name = "lblCambios";
+            this.lblCambios.Size = new System.Drawing.Size(274, 37);
+            this.lblCambios.TabIndex = 14;
+            this.lblCambios.Text = "CONTROL DE CAMBIOS";
+            // 
+            // dgvUsuarios
+            // 
+            this.dgvUsuarios.AllowUserToAddRows = false;
+            this.dgvUsuarios.AllowUserToDeleteRows = false;
+            this.dgvUsuarios.AllowUserToResizeColumns = false;
+            this.dgvUsuarios.AllowUserToResizeRows = false;
+            this.dgvUsuarios.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvUsuarios.BackgroundColor = System.Drawing.Color.Gainsboro;
+            this.dgvUsuarios.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvUsuarios.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.dgvUsuarios.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.DarkGreen;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.Menu;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.DarkGreen;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.Menu;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvUsuarios.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvUsuarios.ColumnHeadersHeight = 45;
+            this.dgvUsuarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(254)))), ((int)(((byte)(221)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.Desktop;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvUsuarios.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvUsuarios.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnF2;
+            this.dgvUsuarios.EnableHeadersVisualStyles = false;
+            this.dgvUsuarios.Location = new System.Drawing.Point(12, 125);
+            this.dgvUsuarios.Name = "dgvUsuarios";
+            this.dgvUsuarios.ReadOnly = true;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(254)))), ((int)(((byte)(221)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvUsuarios.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvUsuarios.RowHeadersVisible = false;
+            this.dgvUsuarios.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dgvUsuarios.RowTemplate.Height = 30;
+            this.dgvUsuarios.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvUsuarios.Size = new System.Drawing.Size(545, 211);
+            this.dgvUsuarios.TabIndex = 15;
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 20;
+            this.bunifuElipse1.TargetControl = this.btnRestaurarUsuario;
+            // 
+            // btnRestaurarUsuario
+            // 
+            this.btnRestaurarUsuario.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRestaurarUsuario.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRestaurarUsuario.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnRestaurarUsuario.Location = new System.Drawing.Point(353, 355);
+            this.btnRestaurarUsuario.Name = "btnRestaurarUsuario";
+            this.btnRestaurarUsuario.Size = new System.Drawing.Size(149, 42);
+            this.btnRestaurarUsuario.TabIndex = 44;
+            this.btnRestaurarUsuario.Text = "RESTAURAR USUARIO";
+            this.btnRestaurarUsuario.UseVisualStyleBackColor = true;
+            this.btnRestaurarUsuario.Click += new System.EventHandler(this.btnRestaurarUsuario_Click);
+            // 
+            // bunifuElipse3
+            // 
+            this.bunifuElipse3.ElipseRadius = 15;
+            this.bunifuElipse3.TargetControl = this.dgvUsuarios;
+            // 
+            // lblBuscarUsuario
+            // 
+            this.lblBuscarUsuario.AutoSize = true;
+            this.lblBuscarUsuario.BackColor = System.Drawing.Color.Gainsboro;
+            this.lblBuscarUsuario.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Italic);
+            this.lblBuscarUsuario.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.lblBuscarUsuario.Location = new System.Drawing.Point(75, 370);
+            this.lblBuscarUsuario.Name = "lblBuscarUsuario";
+            this.lblBuscarUsuario.Size = new System.Drawing.Size(101, 15);
+            this.lblBuscarUsuario.TabIndex = 47;
+            this.lblBuscarUsuario.Text = "Buscar usuario ...";
+            this.lblBuscarUsuario.MouseClick += new System.Windows.Forms.MouseEventHandler(this.lblBuscarUsuario_MouseClick);
+            // 
+            // imgBuscar
+            // 
+            this.imgBuscar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.imgBuscar.Image = global::HayCancha.Properties.Resources.Buscar;
+            this.imgBuscar.Location = new System.Drawing.Point(202, 367);
+            this.imgBuscar.Name = "imgBuscar";
+            this.imgBuscar.Size = new System.Drawing.Size(27, 22);
+            this.imgBuscar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imgBuscar.TabIndex = 46;
+            this.imgBuscar.TabStop = false;
+            this.imgBuscar.Click += new System.EventHandler(this.imgBuscar_Click);
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.BackColor = System.Drawing.Color.Gainsboro;
+            this.txtBuscar.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtBuscar.Font = new System.Drawing.Font("Arial", 11F);
+            this.txtBuscar.Location = new System.Drawing.Point(52, 367);
+            this.txtBuscar.Multiline = true;
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(144, 22);
+            this.txtBuscar.TabIndex = 45;
+            this.txtBuscar.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtBuscar.TextChanged += new System.EventHandler(this.txtBuscar_TextChanged);
+            // 
+            // bunifuElipse4
+            // 
+            this.bunifuElipse4.ElipseRadius = 8;
+            this.bunifuElipse4.TargetControl = this.txtBuscar;
+            // 
+            // UsuariosUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.ForestGreen;
+            this.ClientSize = new System.Drawing.Size(569, 422);
+            this.Controls.Add(this.lblBuscarUsuario);
+            this.Controls.Add(this.imgBuscar);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.btnRestaurarUsuario);
+            this.Controls.Add(this.dgvUsuarios);
+            this.Controls.Add(this.lblCambios);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "UsuariosUI";
+            this.Tag = "41";
+            this.Text = "UsuariosUI";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsuarios)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBuscar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCambios;
+        private System.Windows.Forms.DataGridView dgvUsuarios;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse3;
+        private System.Windows.Forms.Button btnRestaurarUsuario;
+        private System.Windows.Forms.Label lblBuscarUsuario;
+        private System.Windows.Forms.PictureBox imgBuscar;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse4;
+    }
+}
---
+++ HayCancha/HayCancha/Vistas/ControlCambios.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Windows.Forms;
+using HayCancha.BE.Enumerables;
+using HayCancha.BE.Interfaces;
+using HayCancha.BLL;
+using HayCancha.Servicios;
+using Microsoft.VisualBasic;
+
+namespace HayCancha.Vistas
+{
+    public partial class ControlCambios : Form, Idiomable
+    {
+        public ControlCambios()
+        {
+            InitializeComponent();
+            SessionService.Session.Suscribir(this);
+            if (IdiomaControl != SessionService.Session.Idioma) Update();
+            BuscarUsuarioAuditoriaPorNombre(SessionService.Session.UsuarioLogueado.Nombre);
+        }
+
+        public int IdiomaControl { get; set; }
+
+        public void ActualizarControles(IEnumerable Coleccion)
+        {
+            foreach (var oComponente in Coleccion)
+            {
+                ((Control)oComponente).Text = TraductorService.RetornaTraduccion(((Control)oComponente).Text) ?? ((Control)oComponente).Text;
+            }
+        }
+
+        public new void Update()
+        {
+            ActualizarControles(this.Controls);
+        }
+
+        private void lblBuscarUsuario_MouseClick(object sender, MouseEventArgs e)
+        {
+            lblBuscarUsuario.Visible = false;
+            txtBuscar.Focus();
+        }
+
+        private void txtBuscar_TextChanged(object sender, System.EventArgs e)
+        {
+            if (txtBuscar.Text != string.Empty) return;
+            lblBuscarUsuario.Visible = true;
+            lblCambios.Focus();
+        }
+
+        private void imgBuscar_Click(object sender, System.EventArgs e)
+        {
+            BuscarUsuarioAuditoriaPorNombre(txtBuscar.Text);
+        }
+
+        private void btnRestaurarUsuario_Click(object sender, System.EventArgs e)
+        {
+            var usuarioSeleccionado = dgvUsuarios.SelectedRows[0].Cells["Nombre"].Value.ToString();
+
+            var cerrarSession = usuarioSeleccionado == SessionService.Session.UsuarioLogueado.Nombre;
+
+            if (cerrarSession && Interaction.MsgBox("Luego de ejecutar la restauración del usuario se cerrará la sesión. ¿Desea continuar?", MsgBoxStyle.OkCancel, "Restauración") == MsgBoxResult.Cancel) return;
+            
+            UsuarioBLL.RestaurarUsuario((int)dgvUsuarios.SelectedRows[0].Cells["Id"].Value);
+
+            BitacoraService.Escribir(TipoEventoBitacoraEnum.SISTEMA, $"El usuario {usuarioSeleccionado} restauró el perfil a un estado anterior.");
+
+            BuscarUsuarioAuditoriaPorNombre(txtBuscar.Text);
+
+            MessageBox.Show($"El usuario {usuarioSeleccionado} fue restaurado exitosamente!", "ALERTA", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            if (!cerrarSession) return;
+
+            ((MenuUI)Parent.Parent).CerrarSesion();
+            Close();
+        }
+
+        private void BuscarUsuarioAuditoriaPorNombre(string sNombre)
+        {
+            dgvUsuarios.DataSource = UsuarioBLL.ObtenerUsuariosAuditoriaPorNombre(sNombre);
+            dgvUsuarios.Columns["Id"].Visible = false;
+            dgvUsuarios.Columns["Imagen"].Width = 70;
+            dgvUsuarios.Columns["Contraseña"].Width = 70;
+        }
+    }
+}
