35f2ac6f10b382a59d7d623e483410dd6075044a
Lucas Chmelik <lchmelik@cys.com.ar>
2021-11-14 18:28:08 -0300

    Instalador funcionando creo

---
+++ HayCancha/HayCancha/Vistas/PantallaCarga.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+namespace HayCancha.Vistas
+{
+    partial class PantallaCarga
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picCargando = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picCargando)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picCargando
+            // 
+            this.picCargando.ImageLocation = "";
+            this.picCargando.Location = new System.Drawing.Point(169, 138);
+            this.picCargando.Name = "picCargando";
+            this.picCargando.Size = new System.Drawing.Size(125, 104);
+            this.picCargando.TabIndex = 0;
+            this.picCargando.TabStop = false;
+            // 
+            // PantallaCarga
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 408);
+            this.Controls.Add(this.picCargando);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "PantallaCarga";
+            this.Opacity = 0.7D;
+            this.Text = "PantallaCarga";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.PantallaCarga_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picCargando)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picCargando;
+    }
+}
---
+++ HayCancha/HayCancha/Vistas/PantallaCarga.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace HayCancha.Vistas
+{
+    public partial class PantallaCarga : Form
+    {
+        public PantallaCarga()
+        {
+            InitializeComponent();
+        }
+
+        private void PantallaCarga_Load(object sender, EventArgs e)
+        {
+            picCargando.Image = Image.FromFile($"{Environment.CurrentDirectory}\\cargando.gif");
+            picCargando.Load($"{Environment.CurrentDirectory}\\cargando.gif");
+            picCargando.Location = new Point(Width/2 - picCargando.Width/2, Height / 2 - picCargando.Height / 2);
+        }
+    }
+}
