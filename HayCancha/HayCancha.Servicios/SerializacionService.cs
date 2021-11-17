06c3b5c500295dfea4af4d56e1bac600b77103a5
Lucas Chmelik <lchmelik@cys.com.ar>
2021-11-17 16:03:18 -0300

    Se crea SerializacionService

---
+++ HayCancha/HayCancha.Servicios/SerializacionService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using HayCancha.BE;
+
+namespace HayCancha.Servicios
+{ 
+    public static class SerializacionService
+    {
+        //public static object Serializar(object objetoASerializar)
+        //{
+        //    var miStream = new MemoryStream();
+
+        //    new BinaryFormatter().Serialize(miStream, objetoASerializar);
+
+        //    var captura = new UsuarioSerializado()
+        //    {
+        //        objetoSerializacion = miStream.ToArray(),
+        //        FechaModificacion = DateTime.Now,
+        //        usuarioModificacion = SessionService.Session.ObtenerNombreUsuario()
+        //    };
+
+        //    miStream.Close();
+
+        //    return captura;
+        //}
+
+        //public static Usuario Deserializar(Usuario captura)
+        //{
+        //    var memoryStream = new MemoryStream(captura.objetoSerializacion);
+
+        //    return (Usuario)(new BinaryFormatter().Deserialize(memoryStream));
+        //}
+    }
+}
